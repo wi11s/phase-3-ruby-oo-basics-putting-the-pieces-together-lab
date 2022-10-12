ef3ca798a9bad7a769670e594fe88ba38c78d9a9
William <wms274@nyu.edu>
2022-10-12 09:54:16 -0400

    commit tree c93f517ed875576c1a3bbdc6fd1487793d025516

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+class Book
+    def initialize(title = "And Then There Were None", author = "Agatha Christie", page_count = 272, genre = "Mystery")
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
+    end
 
+    attr_accessor :genre
+    attr_accessor :page_count
+    attr_accessor :author
+    attr_accessor :title
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
